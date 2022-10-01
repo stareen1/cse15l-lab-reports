ff4afa5df0e7aac19d9c7fbef32f146fd888e7cb
stareen1 <114516095+stareen1@users.noreply.github.com>
2022-09-30 20:15:50 -0700

    Update WhereAmI.java

--- WhereAmI.java
+++ WhereAmI.java
@@ -1,6 +1,7 @@
 class WhereAmI {
   public static void main(String[] args) {
     System.out.println(System.getProperty("os.name"));
+    System.out.println(System.getProperty("os.name"));
     System.out.println(System.getProperty("user.name"));
     System.out.println(System.getProperty("user.home"));
     System.out.println(System.getProperty("user.home"));
